e2cf1cacbb8f0986ddd5227b6bf900ea665f55c9
AshishCode <ashishmaheshwari7098@gmail.com>
2026-04-25 18:47:09 +0530

    socket.io

--- src/Socket/socket.io.js
+++ src/Socket/socket.io.js
@@ -1,13 +1,39 @@
 import {Server, Socket} from "socket.io";
+import UserModel from "../models/user.js";
 
 function initSocket(server) {
   console.log("Socket.io connected")
-
   const io = new Server(server);
+  try{
+   
+    io.use( async (socket, next) => {
+      const token = socket.handshake.headers.token;
+      if (!token) {
+        return next(new Error("Authentication error"));
+      }
 
-  io.on("connection", (Socket)=>{
-    console.log("User connected")
-  })
+   
+       const decodedToken = UserModel.verifyToken(token);
+  
+          const user = await UserModel.findById(decodedToken._id);
+
+      if (!user) {
+        return next(new Error("User not found"));
+      }
+
+      socket.user = user;
+
+      next();
+    });
+  }
+  catch(err){
+    console.error("Error initializing Socket.io:", err);
+  }
+
+  io.on("connection", (socket) => {
+    console.log(socket.user);
+    console.log("User connected");
+  });
 }
 
 export default initSocket ; 
